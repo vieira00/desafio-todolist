40afdf066234cb2a7fba1087c3ef23d647685138
vieira00 <matheusricardov563@gmail.com>
2024-11-20 11:19:53 -0400

    foi feita uma correcao na classe (TodoRepository).

--- src/main/java/com/matheus/desafio_todolist/repository/TodoRepository.java
+++ src/main/java/com/matheus/desafio_todolist/repository/TodoRepository.java
@@ -3,5 +3,5 @@ package com.matheus.desafio_todolist.repository;
 import com.matheus.desafio_todolist.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class TodoRepository extends JpaRepository<Todo, long> {
+public interface TodoRepository extends JpaRepository<Todo, Long> {
 }
